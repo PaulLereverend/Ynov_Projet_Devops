df2f0a7951187f0d920b5d8121671d50f7f88c4b
HugoHUET <hugo.huet.14@gmail.com>
2020-04-13 14:42:15 +0200

    réponse format json

--- back/src/config.py
+++ back/src/config.py
@@ -1,4 +1,4 @@
 import pymysql
 
 def connect():
-	return pymysql.connect(host='cloudnantespaul.freeboxos.fr', port=3390, user='ynov_devops', password='ynov_devops', db='ynov_devops')
+	return pymysql.connect(host='cloudnantespaul.freeboxos.fr', port=3390, user='ynov_devops', password='ynov_devops', db='ynov_devops', cursorclass=pymysql.cursors.DictCursor)
