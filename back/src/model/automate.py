510f8121da40b932d7eb72b9bec2407e93868a6d
Paul <paulereverend@gmail.com>
2020-04-14 13:21:59 +0200

    Gestion des CORS

--- back/src/model/automate.py
+++ back/src/model/automate.py
@@ -2,39 +2,46 @@ from app import app
 from flask import flash, request
 from config import connect
 import json
+from flask_cors import CORS, cross_origin
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @app.route('/automates', methods=['GET'])
+@cross_origin()
 def get_automates():
-	if request.args.__len__() > 0:
-		num_unite = request.args.get('num_unite')
-		print(num_unite)
-		connection = connect()
-		try:
-			with connection.cursor() as cursor:
-				sql = "SELECT * FROM `automate` where unite_id = %s"
-				cursor.execute(sql, num_unite)
-				return json.dumps(cursor.fetchall())
-		finally:
-			if cursor != None:
-				cursor.close()
-			if connection != None:
-				connection.close()
-	return json.dumps({})
+    if request.args.__len__() > 0:
+        num_unite = request.args.get('num_unite')
+        print(num_unite)
+        connection = connect()
+        try:
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM `automate` where unite_id = %s"
+                cursor.execute(sql, num_unite)
+                return json.dumps(cursor.fetchall())
+        finally:
+            if cursor != None:
+                cursor.close()
+            if connection != None:
+                connection.close()
+    return json.dumps({})
+
 
 @app.route('/automate/data', methods=['GET'])
 def get_data():
-	if request.args.__len__() > 0:
-		num_automate = request.args.get('num_automate')
-		date_fin = request.args.get('date_fin')
-		connection = connect()
-		try:
-			with connection.cursor() as cursor:
-				sql = "SELECT * FROM `automate` where automate_id = %s AND date < now() AND date > %s"
-				cursor.execute(sql, (num_automate, date_fin))
-				return json.dumps(cursor.fetchall())
-		finally:
-			if cursor != None:
-				cursor.close()
-			if connection != None:
-				connection.close()
-	return json.dumps({})
+    if request.args.__len__() > 0:
+        num_automate = request.args.get('num_automate')
+        date_fin = request.args.get('date_fin')
+        connection = connect()
+        try:
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM `automate` where automate_id = %s AND date < now() AND date > %s"
+                cursor.execute(sql, (num_automate, date_fin))
+                return json.dumps(cursor.fetchall())
+        finally:
+            if cursor != None:
+                cursor.close()
+            if connection != None:
+                connection.close()
+    return json.dumps({})
--- back/src/model/unite.py
+++ back/src/model/unite.py
@@ -2,19 +2,25 @@ from app import app
 from flask import flash, request
 from config import connect
 import json
+from flask_cors import CORS, cross_origin
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @app.route('/unites', methods=['GET'])
+@cross_origin()
 def get_unites():
-	connection = connect()
-	try:
-		with connection.cursor() as cursor:
-			sql = "SELECT * FROM `unite`"
-			cursor.execute(sql)
-			return json.dumps(cursor.fetchall())
-	finally:
-		if cursor != None:
-			cursor.close()
-		if connection != None:
-			connection.close()
-	
-	return json.dumps({})
+    connection = connect()
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM `unite`"
+            cursor.execute(sql)
+            return json.dumps(cursor.fetchall())
+    finally:
+        if cursor != None:
+            cursor.close()
+        if connection != None:
+            connection.close()
+
+    return json.dumps({})
